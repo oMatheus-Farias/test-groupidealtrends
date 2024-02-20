8387c1ad568a49ed579f7b45b0c750c1f2fe77fa
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-20 20:41:48 -0300

    feat: add function that handles opening and closing the Sub Menu Primary

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,11 @@
   const $hamburguerMenuIcon = document.querySelector(".header-button-menu");
   const $closeMenuIcon = document.querySelector(".nav-mobile-button-menu");
 
+  const $linkCompanyMenuHeader = document.querySelector(".company");
+  const $sunMenuPrimary = document.querySelector(".subMenuPrimary-content");
+
   let toggleMenu = false;
+  let toggleSubMenuPrimary = false;
 
   const mobileMenuViewControl = () => {
     if (toggleMenu) {
@@ -18,11 +22,28 @@
   };
   mobileMenuViewControl();
 
+  const subMenuPrimaryViewControl = () => {
+    if (toggleSubMenuPrimary) {
+      $sunMenuPrimary.style.visibility = "visible";
+      $sunMenuPrimary.style.opacity = "1";
+    } else {
+      $sunMenuPrimary.style.visibility = "hidden";
+      $sunMenuPrimary.style.opacity = "0";
+    }
+  };
+  subMenuPrimaryViewControl();
+
   const handleToggleMenu = () => {
     toggleMenu = !toggleMenu;
     mobileMenuViewControl();
   };
 
+  const handleToggleSubMenuPrimary = () => {
+    toggleSubMenuPrimary = !toggleSubMenuPrimary;
+    subMenuPrimaryViewControl();
+  };
+
   $hamburguerMenuIcon.addEventListener("click", handleToggleMenu);
   $closeMenuIcon.addEventListener("click", handleToggleMenu);
+  $linkCompanyMenuHeader.addEventListener("click", handleToggleSubMenuPrimary);
 })();
